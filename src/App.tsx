21d780979756974c8268789a8cf78cf0e789d27e
MateusPitura <mateuspitura@gmail.com>
2023-05-13 22:11:42 -0300

    refactor(screens): separado o arquivo da nevegação em dois
    Para melhorar a organização, separado o arquivo da navegação em dois. Com isso foi adicionado estilização da status bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,15 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Main from './screens/Main';
-import About from './screens/About';
-import Hobbies from './screens/Hobbies';
-import Education from './screens/Education';
-import Experience from './screens/Experience';
-
-const BottomTabNavigator = createBottomTabNavigator();
+import Root from './screens/Root'
 
 export default function(){
   return(
     <NavigationContainer>
-      <BottomTabNavigator.Navigator initialRouteName="Main">
-        <BottomTabNavigator.Screen
-          name="About"
-          component={About}
-          options={{
-            title:'SOBRE',
-            headerStyle:{
-              backgroundColor:'#FFA500',
-            },
-            headerTintColor:'#fff',
-            headerTitleStyle:{
-              fontWeight:'bold',
-            },
-            headerTitleAlign:'center',
-          }}
-        />
-        <BottomTabNavigator.Screen
-          name="Hobbies"
-          component={Hobbies}
-          options={{
-            title:'HOBBIES',
-            headerStyle:{
-              backgroundColor:'#FFA500',
-            },
-            headerTintColor:'#fff',
-            headerTitleStyle:{
-              fontWeight:'bold',
-            },
-            headerTitleAlign:'center',
-          }}
-        />
-        <BottomTabNavigator.Screen
-          name="Main"
-          component={Main}
-          options={{
-            title:'MENU PRINCIPAL',
-            headerStyle:{
-              backgroundColor:'#FFA500',
-            },
-            headerTintColor:'#fff',
-            headerTitleStyle:{
-              fontWeight:'bold',
-            },
-            headerTitleAlign:'center',
-          }}
-        />
-        <BottomTabNavigator.Screen
-          name="Education"
-          component={Education}
-          options={{
-            title:'EDUCAÇÃO',
-            headerStyle:{
-              backgroundColor:'#FFA500',
-            },
-            headerTintColor:'#fff',
-            headerTitleStyle:{
-              fontWeight:'bold',
-            },
-            headerTitleAlign:'center',
-          }}
-        />
-        <BottomTabNavigator.Screen
-          name="Experience"
-          component={Experience}
-          options={{
-            title:'EXPERIÊNCIA',
-            headerStyle:{
-              backgroundColor:'#FFA500',
-            },
-            headerTintColor:'#fff',
-            headerTitleStyle:{
-              fontWeight:'bold',
-            },
-            headerTitleAlign:'center',
-          }}
-        />
-      </BottomTabNavigator.Navigator>
+      <StatusBar
+        backgroundColor="#FFA500"
+      />
+      <Root/>
     </NavigationContainer>
   )
 }
---
+++ src/screens/Root.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Main from './Main';
+import About from './About';
+import Hobbies from './Hobbies';
+import Education from './Education';
+import Experience from './Experience';
+
+const BottomTabNavigator = createBottomTabNavigator();
+
+export default function(){
+  return(
+      <BottomTabNavigator.Navigator initialRouteName="Main">
+        <BottomTabNavigator.Screen
+          name="About"
+          component={About}
+          options={{
+            title:'SOBRE',
+            headerStyle:{
+              backgroundColor:'#FFA500',
+            },
+            headerTintColor:'#fff',
+            headerTitleStyle:{
+              fontWeight:'bold',
+            },
+            headerTitleAlign:'center',
+          }}
+        />
+        <BottomTabNavigator.Screen
+          name="Hobbies"
+          component={Hobbies}
+          options={{
+            title:'HOBBIES',
+            headerStyle:{
+              backgroundColor:'#FFA500',
+            },
+            headerTintColor:'#fff',
+            headerTitleStyle:{
+              fontWeight:'bold',
+            },
+            headerTitleAlign:'center',
+          }}
+        />
+        <BottomTabNavigator.Screen
+          name="Main"
+          component={Main}
+          options={{
+            title:'MENU PRINCIPAL',
+            headerStyle:{
+              backgroundColor:'#FFA500',
+            },
+            headerTintColor:'#fff',
+            headerTitleStyle:{
+              fontWeight:'bold',
+            },
+            headerTitleAlign:'center',
+          }}
+        />
+        <BottomTabNavigator.Screen
+          name="Education"
+          component={Education}
+          options={{
+            title:'EDUCAÇÃO',
+            headerStyle:{
+              backgroundColor:'#FFA500',
+            },
+            headerTintColor:'#fff',
+            headerTitleStyle:{
+              fontWeight:'bold',
+            },
+            headerTitleAlign:'center',
+          }}
+        />
+        <BottomTabNavigator.Screen
+          name="Experience"
+          component={Experience}
+          options={{
+            title:'EXPERIÊNCIA',
+            headerStyle:{
+              backgroundColor:'#FFA500',
+            },
+            headerTintColor:'#fff',
+            headerTitleStyle:{
+              fontWeight:'bold',
+            },
+            headerTitleAlign:'center',
+          }}
+        />
+      </BottomTabNavigator.Navigator>
+  )
+}
