b67617267808df35f2e960f556e2846303d72089
MateusPitura <mateuspitura@gmail.com>
2023-05-14 11:20:28 -0300

    feat(screens): adicionado imagens
    Adicionado imagens no Main

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text, ScrollView} from 'react-native'
+import {View, Text, ScrollView, Image} from 'react-native'
 import StyleText from '../style/text'
 import StyleScreen from '../style/screens'
 
@@ -8,7 +8,13 @@ export default function(){
 	return(
 		<ScrollView>
 			<View style={StyleScreen.layout}>
-				<Text style={StyleText.regular}>Este aplicativo foi desenvolvimento a fim de contar um pouco sobre mim. Navegue entre as categorias para me conhecer melhor!{'\n'}</Text>
+				<Text style={StyleText.regular}>Este aplicativo foi desenvolvimento a fim de contar um pouco sobre mim na estrevista para estágio da Continental. Navegue entre as categorias para me conhecer melhor!{'\n'}</Text>
+				<View style={StyleScreen.layoutImages}>
+					<Image 
+						source={require('../assets/images/Foto.png')}
+						style={StyleScreen.images}
+					/>
+				</View>
 			</View>
 		</ScrollView>
 	)
--- src/style/screens.tsx
+++ src/style/screens.tsx
@@ -6,4 +6,13 @@ export default StyleSheet.create({
         paddingBottom: 200,
         backgroundColor: '#fff',
     },
+    layoutImages:{
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    images:{
+        width: 300,
+        height: 225,
+        resizeMode: 'contain',
+    },
 })
